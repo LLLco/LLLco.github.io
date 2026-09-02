b68eca0a23d0946e85c858c6894d4cafdce591a7
LLLco <lrburley@uci.edu>
2026-09-02 04:32:39 -0700

    cleaner code and error catching

--- scripts/navColumnManager.js
+++ scripts/navColumnManager.js
@@ -5,6 +5,7 @@ document.getElementById("MAIN").classList.toggle("hidden");
 
 for (const child of navcolumn.children) {
     //child.id = child.textContent;
+    if (child.id == "BACK") { continue; }
     buttons.push(child)
     child.addEventListener("click", handleClick);
 
@@ -13,11 +14,9 @@ for (const child of navcolumn.children) {
 
 function handleClick() {
     for (const b of buttons) {
-        if (b.textContent != event.target.textContent) {
-            document.getElementById(b.textContent).classList.toggle("hidden", true);
-        }
-        else {
-            document.getElementById(b.textContent).classList.toggle("hidden", false);
-        }
+        const element = document.getElementById(b.textContent);
+        if (element == null) {continue;}
+
+        element.classList.toggle("hidden", (b.textContent != event.target.textContent));
     }
 }
