fd951d31023e405e0da6d72bf38d35e522a9e591
LLLco <lrburley@uci.edu>
2026-09-02 04:14:42 -0700

    yah thats a working button panel lowkey

--- scripts/navColumnManager.js
+++ scripts/navColumnManager.js
@@ -1,1 +1,23 @@
 const navcolumn = document.getElementById("navcolumn");
+var buttons = [];
+
+document.getElementById("MAIN").classList.toggle("hidden");
+
+for (const child of navcolumn.children) {
+    //child.id = child.textContent;
+    buttons.push(child)
+    child.addEventListener("click", handleClick);
+
+    console.log(buttons);
+}
+
+function handleClick() {
+    for (const b of buttons) {
+        if (b.textContent != event.target.textContent) {
+            document.getElementById(b.textContent).classList.toggle("hidden", true);
+        }
+        else {
+            document.getElementById(b.textContent).classList.toggle("hidden", false);
+        }
+    }
+}
