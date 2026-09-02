d31610639e2f3a71ba0cfe7db6a6d190ccd4f907
LLLco <lrburley@uci.edu>
2026-09-02 04:39:30 -0700

    Update navColumnManager.js

--- scripts/navColumnManager.js
+++ scripts/navColumnManager.js
@@ -4,7 +4,6 @@ var buttons = [];
 document.getElementById("MAIN").classList.toggle("hidden");
 
 for (const child of navcolumn.children) {
-    //child.id = child.textContent;
     if (child.id == "BACK") { continue; }
     buttons.push(child)
     child.addEventListener("click", handleClick);
@@ -13,7 +12,7 @@ for (const child of navcolumn.children) {
 function handleClick() {
     for (const b of buttons) {
         const element = document.getElementById(b.textContent);
-        if (element == null) {continue;}
+        if (element == null) { continue; }
 
         element.classList.toggle("hidden", (b.textContent != event.target.textContent));
     }
